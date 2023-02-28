2b7d4d747df3a95e8025a1b3bb7f0c326a8214e1
Oleksandr Voznenko <124575056+Willeyfox-kali@users.noreply.github.com>
2023-02-28 20:19:05 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -36,9 +36,9 @@ def main():
         print(rezult)
     elif n == 'F':
         file = input()
-        with open (file) as f:
-            n = int(file.readline().strip())
-            parents = list(map(int, file.readline().strip().split()))
+        with open (file, 'r') as f:
+            n = int(f.readline().strip())
+            parents = list(map(int, f.readline().strip().split()))
             rezult = compute_height(n, parents)
             print(rezult)
     # implement input form keyboard and from files
